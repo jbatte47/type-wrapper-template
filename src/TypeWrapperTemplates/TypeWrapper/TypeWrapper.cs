de124d7e4376d3365bb988ff0ff328b239fd9205
John Batte <john.batte@icainformatics.com>
2012-12-18 09:38:14 -0600

    Initial TypeWrapper.tt file (from existing project)

---
+++ src/TypeWrapperTemplates/TypeWrapper/TypeWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TypeWrapper
+{
+	#region Interface
+
+	public interface IObject
+	{
+
+	}
+
+	#endregion
+
+	#region Wrapper
+
+	public class ObjectWrapper : IObject
+	{
+		private readonly Object _innerType;
+
+		public ObjectWrapper(Object innerType)
+		{
+			_innerType = innerType;
+		}
+
+	}
+
+	#endregion
+}
